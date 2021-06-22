1c1ddaf14fbe49a20a408a05f013a6074903314d
mrmoon007 <mrmoon025@gmail.com>
2021-06-22 22:18:37 +0600

    frontend mastering done

---
+++ resources/views/frontend/layouts/partials/script.blade.php
@@ -0,0 +1,3 @@
+<script src="{{ asset('frontend/assets/js/jquery-3.3.1.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/popper.min.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/bootstrap.min.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('frontend.pages.index');
+})->name('home');
+
+Route::get('/show',[BookController::class,'show'])->name('show');
+
+//jafsjajs
